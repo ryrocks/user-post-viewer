fd69d0ae564bfc32b300e2cd294df5e59e6292bd
raymond.yeh <raymond.yeh@milkfish.digital>
2022-05-27 15:23:30 +0800

    feat: add Jest and Cypress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default Home;
